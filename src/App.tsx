b7508f785e67bce1fc3cc09e8db0f75d1240160b
Tami <kazehare.t@gmail.com>
2025-03-05 12:25:08 +0000

    skills section created

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/NavBar/Navbar";
+import { TechStack } from "./components/TeckStack/TechStack";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <TechStack />
     </div>
   );
 }
